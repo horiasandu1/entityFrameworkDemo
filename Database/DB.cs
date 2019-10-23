3c2be3492b4f644ba37030c67723cf3516750d93
Horia Cristian Sandu <horiasandu1@gmail.com>
2019-10-23 16:36:59 -0400

    Add project files.

---
+++ Database/DB.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public static class DB
+    {
+
+        private static Demo1Entities myDb = new Demo1Entities();
+
+        public static IEnumerable<Product> GetProducts()
+        {
+            return myDb.Products.ToList();
+        }
+        public static Product GetProduct (int id)
+        {
+            return myDb.Products.Find(id);
+            
+        }
+
+        public static void addProduct(string prodDesc, string prodUpc)
+        {
+            Product newProd = new Product();
+            
+            newProd.ProductDescription = prodDesc;
+            newProd.ProductUpc = prodUpc;
+            myDb.Products.Add(newProd);
+            myDb.SaveChanges();
+        }
+
+        public static IEnumerable<Customer> getCustomers()
+        {
+            return myDb.Customers.ToList();
+        }
+
+        public static Customer getCustomer(int id)
+        {
+            return myDb.Customers.Find(id);
+        }
+
+        public static void addCustomer(string cusFName, string cusLName)
+        {
+            Customer newCus = new Customer();
+            newCus.CustomerFirstName = cusFName;
+            newCus.CustomerLastName = cusLName;
+            myDb.Customers.Add(newCus);
+            myDb.SaveChanges();
+        }
+
+        public static IEnumerable<Sale> getSales()
+        {
+            return myDb.Sales.ToList();
+        }
+
+        public static Sale getSale(int id)
+        {
+            return myDb.Sales.Find(id);
+        }
+
+        public static void addSale(string preConDate,Product p, Customer c)
+        {
+            Sale newSale = new Sale();
+            DateTime postConDate = Convert.ToDateTime(preConDate);
+
+            newSale.SaleDate = postConDate;
+            newSale.ProductId = p.ProductId;
+            newSale.CustomerId = c.CustomerId;
+
+            myDb.Sales.Add(newSale);
+            myDb.SaveChanges();
+        }
+
+        public static void updateCustomer(int id,string cusFName, string cusLName)
+        {
+            Customer cust = getCustomer(id);
+            cust.CustomerFirstName = cusFName;
+            cust.CustomerLastName = cusLName;
+
+            myDb.Entry(cust).State = System.Data.Entity.EntityState.Modified;
+            myDb.SaveChanges();
+
+        }
+        
+    }
+}
---
+++ entityFrameworkDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database;
+namespace entityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var salesList = DB.getSales();
+
+            for (var i = 0; i < salesList.Count(); i++)
+            {
+
+                Console.WriteLine(salesList.ElementAt(i).SaleId + " " 
+                    + DB.getCustomer(salesList.ElementAt(i).CustomerId).CustomerFirstName + " " +
+           DB.getCustomer(salesList.ElementAt(i).CustomerId).CustomerLastName + " " + DB.GetProduct(salesList.ElementAt(i).ProductId).ProductDescription);
+                Console.WriteLine();
+            }
+
+            
+
+            //  Database.DB.updateCustomer(3, "Lea", "Sandu");
+
+            // Database.DB.addCustomer("Lea", "Ambrosoli");/*
+            var myCustomer = Database.DB.getCustomer(3);
+
+                       //Console.Write(myCustomer.CustomerLastName);
+                    
+
+             //  Database.DB.addSale("01/01/2011", Database.DB.GetProduct(6), Database.DB.getCustomer(2));
+
+        }
+    }
+}
